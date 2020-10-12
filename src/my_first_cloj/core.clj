dd1016aae6d7de16ef12cac4f79eccc645704985
Bill Shanahan <iamshanahan@gmail.com>
2020-10-12 10:27:28 -0700

    Initial commit

---
+++ src/my_first_cloj/core.clj
@@ -0,0 +1,7 @@
+(ns my-first-cloj.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "I'm a little...darn I'm a sugar bowl."))
---
+++ test/my_first_cloj/core_test.clj
@@ -0,0 +1,7 @@
+(ns my-first-cloj.core-test
+  (:require [clojure.test :refer :all]
+            [my-first-cloj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
