67b29e7387e782e7261444df8ab3f9a904e60cdf
Bill Shanahan <iamshanahan@gmail.com>
2020-10-17 13:50:11 -0700

    trivial unit tests

--- test/my_first_cloj/core_test.clj
+++ test/my_first_cloj/core_test.clj
@@ -2,6 +2,20 @@
   (:require [clojure.test :refer :all]
             [my-first-cloj.core :refer :all]))
 
+(deftest type-test
+  (testing "type"
+    (is (= java.lang.Long (type 1)))
+    (is (= java.lang.String (type "one")))
+    (is (= clojure.lang.PersistentVector (type [1 2 3])))
+    (is (= clojure.lang.PersistentVector (type ["one" "two" "three"])))
+))
+
+(deftest type-test
+  (testing "basic"
+    (is (= 3(+ 2 1)))
+))
+
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
