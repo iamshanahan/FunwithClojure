413e5a554562e4de565c5d42d3b9541f8dfdc163
Bill Shanahan <iamshanahan@gmail.com>
2020-12-15 14:10:19 -0800

    small progress on seq funcs

--- test/my_first_cloj/take_test.clj
+++ test/my_first_cloj/take_test.clj
@@ -11,3 +11,29 @@
     (is (= '(1 2) (take 5 [1 2])))
 ))
 
+(deftest let-test
+  (testing "let"
+    (is (= 6 (let [x 3] (* 2 x))))
+    (is (= 6 (let [[x y] [2 3]] (* x y))))
+    (is (= 9 (let [x 2 [y z] [3 4]] (+ x y z))))
+    (is (= 6 (let [[x y] [2 3 4]] (* x y))))
+    (is (= 6 (let [x (* 2 3)] x)))
+))
+
+(deftest first-test
+  (testing "first"
+    (is (= "first" (first ["first" "second" "third"])))
+    (is (= ["second" "third"] (rest ["first" "second" "third"])))
+    (is (= ["new" "first" "second"] (cons "new" ["first" "second"])))
+))
+
+(deftest map-test
+  (testing "map"
+    (is (= '(3 4 5) (map (fn [x] (+ 1 x)) '(2 3 4) )))
+    (is (= '(3 4 5) (map (fn [x] (+ 1 x)) [2 3 4] )))
+    (is (= '(3 4 5) (map (fn [x] (+ 1 x)) #{2 3 4} )))
+))
+
+(deftest into-test
+  (testing "into"
+))
